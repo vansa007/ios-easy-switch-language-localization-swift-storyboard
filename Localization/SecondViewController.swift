9d0e7a49744edf476623ebd007d711bd679e9e1f
완사 🇰🇭 파 🇰🇷 <vs.lov.rs@gmail.com>
2018-01-26 09:49:03 +0700

    init

---
+++ Localization/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  Localization
+//
+//  Created by Vansa Pha on 1/26/18.
+//  Copyright © 2018 Vansa Pha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismissAction(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -9,17 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myName: UILabel!
+    let preferredLanguage = Locale.current.languageCode
+    var lanString: String = ""
+    let myUser = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let myLan = myUser.string(forKey: "myLan") {
+            lanString = myLan
+        }else {
+            lanString = preferredLanguage!
+        }
+        myName.text = "vansa".localized(lang: lanString)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func englishAction(_ sender: UIButton) {
+        myUser.set("en", forKey: "myLan")
+        myUser.synchronize()
+        //self.performSegue(withIdentifier: "cbr", sender: nil)
+        reloadStoryBoard(lan: myUser.string(forKey: "myLan")!)
+    }
+    
+    @IBAction func koreanAction(_ sender: UIButton) {
+        myUser.set("ko", forKey: "myLan")
+        myUser.synchronize()
+        //self.performSegue(withIdentifier: "cbr", sender: nil)
+        reloadStoryBoard(lan: myUser.string(forKey: "myLan")!)
+    }
+    
+    func reloadStoryBoard(lan: String) {
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        let storyBoardName = "Main"
+        let path = Bundle.main.path(forResource: lan, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        let sb = UIStoryboard(name: storyBoardName, bundle: bundle)
+        delegate.window?.rootViewController = sb.instantiateInitialViewController()
+    }
 }
 
+extension String {
+    
+    func localized(lang: String) -> String {
+        let path = Bundle.main.path(forResource: lang, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+    }
+    
+}
